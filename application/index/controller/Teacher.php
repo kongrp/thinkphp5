7f60de50af34b505ad77330b2e51a89b7b4dee99
kongrp <960139253@qq.com>
2016-06-23 15:32:06 +0800

    获取教师表中所有数据并查看

--- application/index/controller/Teacher.php
+++ application/index/controller/Teacher.php
@@ -1,11 +1,19 @@
 <?php
 namespace app\index\controller;
 use think\Db;  //引用数据库操作类
+/**
+ * 教师管理
+ */
 
 class Teacher
-{
+{	
+	//良好的注释习惯会使我们的编码事半功倍。
 	public function index()
 	{
-		return 'Hello Teacher!';
+		//获取教师表中所有数据
+		$teachers = Db::name('teacher')->select();
+
+		//查看获取的数据
+		var_dump($teachers);
 	}
 }
