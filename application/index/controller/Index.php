791cea685fa3606fa1f998eb3131d643061194c0
kongrp <960139253@qq.com>
2016-06-23 13:01:17 +0800

    Revert "小练习1：访问以下地址，要求输出结果：hello yunzhi"
    This reverts commit 840d2f80726b3d3c8ea5a2a153a37512801bf1ae.

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -2,16 +2,9 @@
 namespace app\index\controller;
 
 class Index
-{	
-	//我的方法名叫index
+{
 	public function index()
 	{
 		return 'Hello World!';
-	}
-
-	//test方法,输出hello yunzhi
-	public function test()
-	{
-		return 'hello yunzhi';
 	}
 }
