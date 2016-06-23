90c774c2a217553ed1ae9839c77cf3be13dada53
kongrp <960139253@qq.com>
2016-06-23 16:06:24 +0800

    间接读取数据-新建teacher类
    位于app\index\controller下，上述是直接读取数据；先删除以上方式的代码，新建间接读取的代码，首先写正式代码前先写测试代码。

--- application/index/controller/Teacher.php
+++ application/index/controller/Teacher.php
@@ -1,27 +1,12 @@
 <?php
 namespace app\index\controller;
-use think\Db;  //引用数据库操作类
 /**
  * 教师管理
  */
 class Teacher
 {	
-	//良好的注释习惯会使我们的编码事半功倍。
 	public function index()
 	{
-		//获取教师表中所有数据
-		$teachers = Db::name('teacher')->select();
-
-		//查看获取的数据
-		var_dump($teachers);
-
-		//输出$teachers中的0号元素
-		var_dump($teachers[0]);
-
-		//输出$teachers中0号元素中的name值
-		var_dump($teachers[0]['name']);
-		//如果变量的类型是个字符串，还可使用echo语句直接输出name值
-		echo $teachers[0]['name'];
-
+		return 'hello teacher!';
 	}
 }
