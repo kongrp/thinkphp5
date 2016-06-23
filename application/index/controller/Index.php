52901542e8115141cd77cad554500cdf82929da6
kongrp <960139253@qq.com>
2016-06-23 14:24:06 +0800

    连接数据库
    获取数据表中的第一条数据

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,12 +1,14 @@
 <?php
 namespace app\index\controller;
+use think\Db; //引用数据库操作类
 
 class Index
 {	
 	//我的方法名叫index
 	public function index()
 	{
-		return 'Hello World!';
+		var_dump(Db::name('teacher')->find()); //获取数据表中第一条数据
+
 	}
 
 	//test方法,输出hello yunzhi
