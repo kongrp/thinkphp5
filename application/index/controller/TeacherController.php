c3afbc76dc1b9bb01d78797130161a86f2d0f90a
kongrp <960139253@qq.com>
2016-06-23 17:23:46 +0800

    3.2.4 间接读取-统一规范代码书写格式（完）

--- application/index/controller/TeacherController.php
+++ application/index/controller/TeacherController.php
@@ -8,15 +8,21 @@ class TeacherController
 {	
 	public function index()
 	{
-		$JiaoShiBiao = new Teacher;
-        $SuoYouJiaoShi = $JiaoShiBiao->select();
+		// $Teacher 首写字大写，说明它是一个对象, 更确切一些说明这是基于Teacher这个模型被我们手工实例化的，如果存在teacher数据表的话，它对应teacher数据表
+		$Teacher = new Teacher;
 
-        // 获取第0个数据
-        $jiaoShiZhangSan = $SuoYouJiaoShi[0];
+		 // $teachers 以s结尾，表示它是一个数组，数据中的每一项都是一个对象，这个对象基于Teahcer这个模型。
+		$teachers = $Teacher->select();
 
-        // 调用上述对象的getData()方法
-        echo '教师姓名' . $jiaoShiZhangSan->getData('name') . '<br />';
-        return '重复一遍:教师姓名' . $jiaoShiZhangSan->getData('name');
+		//获取第0个数据
+		$teacher = $teachers[0];
+
+		//调用上述对象的getData()方法，并传参
+		var_dump($teacher->getData('name'));
+
+		//并增加另外两个直接显示数据的方式
+		return $teacher->getData('name');
+		echo $teacher->getData('name');
 
 	}
 }
