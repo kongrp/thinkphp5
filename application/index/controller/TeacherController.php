eaac0e207f1621515605816b1829fdbe3a0d0549
kongrp <960139253@qq.com>
2016-06-23 17:11:18 +0800

    调用上述对象的getData()方法，并传参

--- application/index/controller/TeacherController.php
+++ application/index/controller/TeacherController.php
@@ -14,5 +14,13 @@ class TeacherController
 		//获取第0个数据
 		$teacher = $teachers[0];
 
+		//调用上述对象的getData()方法，并传参
+		var_dump($teacher->getData('name'));
+
+		//并增加另外两个直接显示数据的方式
+		echo $teacher->getData('name');
+		return $teacher->getData('name');
+
+
 	}
 }
