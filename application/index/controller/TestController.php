0fa44e6312ba9ff5eaae9a5cfd19e2be360ccddf
kongrp <kongrp@kongruipingdeMacBook-Air.local>
2016-06-25 20:38:12 +0800

    继续写测试方法测试验证信息
    1、测试用户名是否存在
    2、测试用户名长度是否符合实际情况

--- application/index/controller/TestController.php
+++ application/index/controller/TestController.php
@@ -4,11 +4,34 @@ use think\Controller;
 
 class TestController extends Controller
 {
+	//测试规则是否符合实际
 	public function index()
     {
         $data = array();
-        $data['username'] = '';
+        $data['username'] = '1';
         $data['name'] = '11';
+        $data['sex'] = '1';
+        $data['email'] = 'hello@hello.com';
+        var_dump($this->validate($data, 'Teacher'));
+    }
+
+    //测试用户名是否存在
+    public function test()
+    {
+        $data = array();
+        $data['username'] = '1';
+        $data['name'] = '1';
+        $data['sex'] = '1';
+        $data['email'] = 'hello@hello.com';
+        var_dump($this->validate($data, 'Teacher'));
+    }
+
+    //测试用户名长度是否符合实际
+    public function test1()
+    {
+        $data = array();
+        $data['username'] = 'ce';
+        $data['name'] = '1';
         $data['sex'] = '1';
         $data['email'] = 'hello@hello.com';
         var_dump($this->validate($data, 'Teacher'));
