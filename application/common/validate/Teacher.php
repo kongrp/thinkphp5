b0724cb1c7cad79db7a1105c4e406104b0aa1569
kongrp <kongrp@kongruipingdeMacBook-Air.local>
2016-06-25 20:30:23 +0800

    建立验证规则

--- application/common/validate/Teacher.php
+++ application/common/validate/Teacher.php
@@ -7,14 +7,14 @@ class Teacher extends Validate
 	// 当前验证的规则
     protected $rule = [
     	'email' => 'email',
-    	'name'  => 'require',
-    	'username'  => 'require',
-    	'sex'  => 'require',
-    	'email'  => 'require',
+    	'name'  => 'require|length:2,25',
+    	'username'  => 'require|unique:teacher|length:4,25',
+    	'sex'  => 'in:0,1',
 
     	    ];
     // 验证提示信息
     protected $message = [
     	'email' => '邮箱格式有误',
+    	'name.require'  => 'name不能为空',
     ];
 }
