c42c71bd2fb982620959e2b95d6d8f1db0ea80d0
kongrp <960139253@qq.com>
2016-07-01 15:22:17 +0800

    index()实现：添加C层代码，以及V层登录form
    由于当前action需要用户输入信息，与用户进行交互，所以必然需要V层的配合。
    
    我们有form，就会有action和method，action是指将数据提交到哪儿。method默认为get，我们在进行非查询操作时，往往使用post方法来保护用户传输的数据。
    在使用ThinkPHP的过程中，凡是涉及到页面跳转的，全部需要借助url()函数进行处理，form表单中的action也不例外。

--- application/index/controller/LoginController.php
+++ application/index/controller/LoginController.php
@@ -2,17 +2,19 @@
 namespace app\index\Controller;
 use think\Controller;
 
-class LoginController
+class LoginController extends Controller
 {	
 	//用户登录表单
 	public function index()
 	{
 		//显示用户登录表单
+		return $this->fetch();
 	}
 
 	//处理用户提交的登录数据
 	public function login()
 	{
+		echo 'hello';
 		// 验证用户名是否存在
 		// 验证密码是否正确
 		// 用户名密码正确 ，将teacherId 存session
