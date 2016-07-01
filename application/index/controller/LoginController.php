48bc5111eb34df7076d38c6c2428039a0d3499f7
kongrp <960139253@qq.com>
2016-07-01 16:32:13 +0800

    4.4.4 引入M层，改写C层进行迁移
    不错，上面的确没有实现登录的功能。但是，这对C层的人来说，已经够了。对于C层的人而言，返回了BOOL类型的值，就可以了。至于M层怎么实现，是M层负责人的问题。这就实现了分工。当然了，我们现在既是C层负责人，又是M层负责人。
    return true：登录成功；false登录失败。

--- application/index/controller/LoginController.php
+++ application/index/controller/LoginController.php
@@ -17,21 +17,12 @@ class LoginController extends Controller
 	//处理用户提交的登录数据
 	public function login()
 	{
-		// 验证用户名是否存在
-		$map = array('username'=>input('post.username'));
-		$Teacher = Teacher::get($map);
-
-		//像我以前说过的，我们每调用一个方法，都要非常清晰的知道它返回值类型是什么。
-        // $Teacher要么是一个对象，要么是false。
-		if(false !== $Teacher && $Teacher->getData('password') === input('post.password'))
+		//直接调用M层方法，进行登录
+		if(Teacher::login(input('post.username'),input('post.password')))
 		{
-			// 用户名密码正确，将teacherId存session
-			session('teacherId',$Teacher->getData('id'));
 			return $this->success('login success',url('Teacher/index'));
 		} else{
-			return $this->error('username or password incorrect',url(index));
+			return $this->error('username or password incorrect',url('index'));
 		}
-		
-		
 	}
 }
--- application/model/Teacher.php
+++ application/model/Teacher.php
@@ -2,9 +2,15 @@
 namespace app\model;
 use think\Model;    // 使用前进行声明
 /**
- * Teacher 教师表
- */
+* 用户登录
+* @param  string $username 用户名
+* @param  string $password 密码
+* @return bool  成功返回true，失败返回false。
+*/
 class Teacher extends Model
 {
-
+	static public function login($username,$password)
+	{
+		return true;
+	}
 }
