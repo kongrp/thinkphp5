aa8e67a3135a7513e9350cf055ebe38bd323d50e
kongrp <960139253@qq.com>
2016-07-01 15:50:37 +0800

    login()代码实现
    // 验证用户名是否存在
    // 验证密码是否正确
    // 用户名密码正确 ，将teacherId 存session
    // 用户名密码错误，跳转到登录界面

--- application/index/controller/LoginController.php
+++ application/index/controller/LoginController.php
@@ -1,7 +1,10 @@
 <?php
 namespace app\index\Controller;
 use think\Controller;
-
+use app\model\Teacher;   //引用教师模型
+/**
+ * 用户登录
+ */
 class LoginController extends Controller
 {	
 	//用户登录表单
@@ -16,8 +19,25 @@ class LoginController extends Controller
 	{
 		var_dump(input('post.'));
 		// 验证用户名是否存在
-		// 验证密码是否正确
-		// 用户名密码正确 ，将teacherId 存session
-		// 用户名密码错误，跳转到登录界面 
+		$map = array('username'=>input('post.username'));
+		$Teacher = Teacher::get($map);
+
+		//像我以前说过的，我们每调用一个方法，都要非常清晰的知道它返回值类型是什么。
+        // $Teacher要么是一个对象，要么是false。
+		if(false !== $Teacher)
+		{
+			// 验证密码是否正确
+			if($Teacher->getData('password') !== input('post.password'))
+			{
+				return $this->error('password incrrect',url(index));
+			} else{
+				// 用户名密码正确，将teacherId存session
+				session('teacherId',$Teacher->getData('id'));
+				return $this->success('login success',url('teacher/index'));
+			}
+		} else{
+			// 用户名密码错误，跳转到登录界面
+			return $this->error('username not exist',url('index'));
+		} 
 	}
 }
