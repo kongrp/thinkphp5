adc2a0b40ecc25e593d32f797da06c9362e662b3
kongrp <960139253@qq.com>
2016-07-01 15:11:18 +0800

    按照流程分别添加注释

--- application/index/controller/LoginController.php
+++ application/index/controller/LoginController.php
@@ -3,14 +3,19 @@ namespace app\index\Controller;
 use think\Controller;
 
 class LoginController
-{
+{	
+	//用户登录表单
 	public function index()
 	{
-		echo 'hello';
+		//显示用户登录表单
 	}
 
+	//处理用户提交的登录数据
 	public function login()
 	{
-		echo 'hi';
+		// 验证用户名是否存在
+		// 验证密码是否正确
+		// 用户名密码正确 ，将teacherId 存session
+		// 用户名密码错误，跳转到登录界面 
 	}
 }
