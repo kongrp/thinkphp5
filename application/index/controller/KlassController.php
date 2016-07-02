c78ece0bd5fea286b2e9e20d9d64eca865a8ef28
kongrp <kongrp@kongruipingdeMacBook-Air.local>
2016-07-02 15:08:41 +0800

    6.2新建数据表对应的模型类、控制器、方法及V层
    进行简单的测试

---
+++ application/index/controller/KlassController.php
@@ -0,0 +1,12 @@
+<?php
+namespace app\index\Controller;
+
+
+class KlassController extends IndexController
+{
+	public function index()
+	{
+		return $this->fetch();
+	}
+	
+}
---
+++ application/model/Klass.php
@@ -0,0 +1,10 @@
+<?php
+namespace app\model;
+use think\Model;
+/**
+ * 班级
+ */
+class Klass extends Model
+{
+
+}
