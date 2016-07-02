be592adedfd366135152b94c70fd672f15cfdd93
kongrp <kongrp@kongruipingdeMacBook-Air.local>
2016-07-02 23:34:34 +0800

    6.4编辑数据-edit和update方法及V层修改

--- application/index/controller/KlassController.php
+++ application/index/controller/KlassController.php
@@ -51,4 +51,49 @@ class KlassController extends IndexController
 		}
 
 	}
+
+	public function edit()
+	{
+		$id = input('get.id/d');
+
+		//获取所有的教师信息
+		$teachers = Teacher::all();
+		$this->assign('teachers',$teachers);
+
+		$Klass = Klass::get($id);
+
+
+		if(false === $Klass)
+		{
+			return $this->error('系统未找到ID为' . $id . '的记录。');
+		}
+
+		$this->assign('Klass',$Klass);
+		return $this->fetch();
+	}
+
+	public function update()
+	{
+		$id = input('post.id');
+
+		//获取传入的班级信息
+		$Klass = Klass::get($id);
+		if(false === $Klass)
+		{
+			return $this->error('系统未找到ID为' . $id . '的记录。');
+		}
+
+		//数据更新
+		$Klass->name = input('post.name');
+		$Klass->teacher_id = input('post.teacher_id');
+		if(false === $Klass->validate()->save())
+		{
+			return $this->error('更新错误' . $Klass->getError());
+		} else{
+			return $this->success('更新成功','index');
+		}
+
+
+
+	}
 }
