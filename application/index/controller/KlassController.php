e9a205ee83e4f2f014331692bc230ac0605c190b
kongrp <kongrp@kongruipingdeMacBook-Air.local>
2016-07-02 15:58:45 +0800

    6.3补充查询信息并为新增操作添加add和save方法

--- application/index/controller/KlassController.php
+++ application/index/controller/KlassController.php
@@ -7,8 +7,37 @@ class KlassController extends IndexController
 {
 	public function index()
 	{
-		$klasses = Klass::paginate(2);
-		$this->assign('klasses',$klasses);
+		//获取查询信息
+		$name = input('get.name');
+		echo $name;
+
+        $pageSize = 2; // 每页显示5条数据
+
+        $Klass = new Klass; 
+
+        // 调用分页
+        $klasses = $Klass->where('name','like','%'.$name.'%')->paginate($pageSize);
+
+        // 向V层传数据
+        $this->assign('klasses',$klasses);
+
+        // 取回打包后的数据
+        $htmls = $this->fetch();
+
+        // 将数据返回给用户
+        return $htmls;	
+	}
+
+	public function add()
+	{
+		//$klasses = Klass::get($id);
+
+		//$this->assign('klasses',$klasses);
 		return $this->fetch();
+	}
+
+	public function save()
+	{
+		var_dump(input('post.'));
 	}
 }
