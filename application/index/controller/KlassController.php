1dae980fe103b039c090de3e03e9538a78bbfdcb
kongrp <kongrp@kongruipingdeMacBook-Air.local>
2016-07-02 15:26:19 +0800

    6.2补充C层和V层代码
    输出班级列表

--- application/index/controller/KlassController.php
+++ application/index/controller/KlassController.php
@@ -1,12 +1,14 @@
 <?php
 namespace app\index\Controller;
+use app\model\Klass;
 
 
 class KlassController extends IndexController
 {
 	public function index()
 	{
+		$klasses = Klass::paginate();
+		$this->assign('klasses',$klasses);
 		return $this->fetch();
 	}
-	
 }
