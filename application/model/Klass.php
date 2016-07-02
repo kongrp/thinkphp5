f6f39db3efa02e9ba1afe3e885752dfe85559112
kongrp <kongrp@kongruipingdeMacBook-Air.local>
2016-07-02 15:28:06 +0800

    6.2修改M层和V层，使得辅导员显示其名称
    直接输入班级列表信息中，辅导员信息是显示teacher_id,需实现具体名称，给出相应的修改。

--- application/model/Klass.php
+++ application/model/Klass.php
@@ -6,5 +6,16 @@ use think\Model;
  */
 class Klass extends Model
 {
+	/**
+     * 获取对应的教师（辅导员）信息
+     * @return Teacher 教师
+     * @author kongrp
+     */
+	public function getTeacher()
+	{
+		$teacherId = $this->getData('teacher_id');
+		$teacher = Teacher::get($teacherId);
+		return $teacher;
+	}
 
 }
