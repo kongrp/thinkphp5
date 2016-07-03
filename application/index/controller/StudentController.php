9f6dcb97e5bf5814aa5b557216e3aa64b9666438
kongrp <kongrp@kongruipingdeMacBook-Air.local>
2016-07-03 17:48:23 +0800

    7.3编辑数据-老方法实现

--- application/index/controller/StudentController.php
+++ application/index/controller/StudentController.php
@@ -1,6 +1,7 @@
 <?php
 namespace app\index\Controller;
 use app\model\Student;
+use app\model\Klass;
 
 class StudentController extends IndexController
 {
@@ -9,5 +10,28 @@ class StudentController extends IndexController
 		$students = Student::paginate();
 		$this->assign('students',$students);
 		return $this->fetch();
+	}
+
+	public function edit()
+	{
+		$id = input('get.id/d');
+
+		//判断是否存在当前记录
+		if(false === $Student = Student::get($id))
+		{
+			return $this->error('未找到ID为' . $id . '的记录');
+		}
+
+		//获取班级列表
+		$klasses = Klass::all();
+		$this->assign('klasses', $klasses);
+	
+		$this->assign('Student', $Student);
+		return $this->fetch();
+	}
+
+	public function update()
+	{
+		
 	}
 }
