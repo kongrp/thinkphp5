40a380dfe2ded1371b1bbee2c2837b9bc292876e
kongrp <kongrp@kongruipingdeMacBook-Air.local>
2016-07-03 14:01:20 +0800

    7.2.1显示性别-在M层实现
    在V层用eq：从思索上的讲度来讲，的确比较简单。但代码还不够清晰。
    修改为thinkphp中的解决方法：在M层中实现，并修改V层为$Student->sex

--- application/model/Student.php
+++ application/model/Student.php
@@ -4,5 +4,20 @@ use think\Model;
 
 class Student extends Model
 {
-
+	/**
+	 * 输出性别的属性
+	 * @return string 0男 1女
+	 * @author kongrp <kongruiping@yunzhiclub.com>
+	 */
+	public function getSexAttr($value)
+	{
+		$status = array('0'=>'男', '1'=>'女');
+		$sex = $status[$value];
+		if(isset($sex))
+		{
+			return $sex;
+		} else{
+			return $status[0];
+		}
+	}
 }
