af960da99db8043bf0c5d54a03f539e8d60f6a4a
kongrp <960139253@qq.com>
2016-07-05 15:11:24 +0800

    7.4魔法函数处理-访问私有或保护属性
    当调用hi这个属性时，由于并不存在可以直接供我们调用的hi属性，那么自动执行了__get()方法，并将$name的值置为'hi'，
    然后执行下面的语句。

--- public/test.php
+++ public/test.php
@@ -4,13 +4,20 @@ header("Content-Type:text/html;charset=utf-8");
 
 class Test
 {
-    private $hi ='hi';
-    public function getHi()
-    {
-        return $this->hi;
-    }
+    private $hi ='hello yunzhi';
+    //历史处理方法
+    // public function getHi()
+    // {
+    //     return $this->hi;
+    // }
 
+    //采用魔法函数
+    public function __get($name)
+    {
+        echo $name . '<br />';
+        echo $this->hi;
+    }
 }
 
 $Test = new Test();
-echo $Test->getHi();
+echo $Test->hi;
