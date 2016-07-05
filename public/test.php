bee2dfca6d8fd0e8e5e2d1c7de2f8988e0c0e119
kongrp <960139253@qq.com>
2016-07-05 15:25:20 +0800

    7.4 魔法函数-示例代码
    thinkphp5在获取一个对象后，可以直接将数据表中的字段值做为属性输出，就是利用的这个方法。

--- public/test.php
+++ public/test.php
@@ -1,23 +1,31 @@
 <?php
-
 header("Content-Type:text/html;charset=utf-8");
 
 class Test
 {
-    private $hi ='hello yunzhi';
-    //历史处理方法
-    // public function getHi()
-    // {
-    //     return $this->hi;
-    // }
+    private $data = array(
+        'name' => '张三',
+        'sex' => '0'
+     );
 
-    //采用魔法函数
     public function __get($name)
     {
-        echo $name . '<br />';
-        echo $this->hi;
+        // 较验在$this->data中是否存在这个健值。
+        // 如果存在，即返回，如果不存在，则返回$this->data整个数组。
+        if(array_key_exists($name, $this->data))
+        {
+            return $this->data[$name];
+        } else {
+            return $this->data;
+        }
     }
 }
 
-$Test = new Test();
-echo $Test->hi;
+    $Test = new Test();
+    echo $Test->name;
+    echo '<br />';
+    echo $Test->sex;
+    var_dump($Test->hello);
+
+
+
