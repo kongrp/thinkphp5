ef085d110caa97d8090fd77e94ab87604bc1ab2b
kongrp <960139253@qq.com>
2016-07-05 15:01:29 +0800

    7.4魔法函数-输出共有属性的值
    把$hi改为私有private或是保护类型ptotected时就会报错。

--- public/test.php
+++ public/test.php
@@ -2,30 +2,10 @@
 
 header("Content-Type:text/html;charset=utf-8");
 
-class Father
+class Test
 {
-    public function __construct()
-    {
-        echo 'Father construct';
-    }
+    public $hi ='hi';
 }
 
-class Son extends Father
-{
-    public function __construct()
-    {
-        // 先执行父类构造函数
-        //parent::__construct();
-
-        echo 'Son construct';
-        // 后执行父类构造函数
-        parent::__construct();
-    }
-}
-
-$br = '<br />';
-$Father = new Father;
-
-echo $br;
-
-$Son = new Son;
+$Test = new Test();
+echo $Test->hi;
