515f3f19401c15b4f4356e4293f740a2a2e52e28
kongrp <960139253@qq.com>
2016-07-05 15:54:53 +0800

    7.4魔法函数__set()
    在thinkphp5中，我们在进行数据的新增或更新时，会直接对一个对象的属性进行赋值。我们当然知道，在以前没有接触到魔法函数以前，如果这个类中并不存在这个属性，或是这个属性不是public类型，那么我们直接赋值就会发生一个异常，__set()就是来解决这个问题的。

--- public/test.php
+++ public/test.php
@@ -3,29 +3,35 @@ header("Content-Type:text/html;charset=utf-8");
 
 class Test
 {
-    private $data = array(
-        'name' => '张三',
-        'sex' => '0'
-     );
+    private $data = array();
 
     public function __get($name)
     {
         // 较验在$this->data中是否存在这个健值。
-        // 如果存在，即返回，如果不存在，则返回$this->data整个数组。
+        // 如果存在，即返回，如果不存在，则提示该字段不存在。
         if(array_key_exists($name, $this->data))
         {
             return $this->data[$name];
         } else {
-            return $this->data;
+            return '字段' . $name . '不存在';
         }
+    }
+
+    public function __set($name, $value)
+    {
+        echo '$name is ' . $name . ', $value is ' . $value . '<br />';
+        $this->data[$name] = $value;
     }
 }
 
     $Test = new Test();
-    echo $Test->name;
-    echo '<br />';
-    echo $Test->sex;
-    var_dump($Test->hello);
+    $Test->name = '李四';
+    $Test->sex = '1';
+    echo $Test->name . '<br />';
+    echo $Test->sex . '<br />';
+    
+    //输出并不存在的hello字段
+    echo $Test->hello;
 
 
 
